fe7d475b127caac8265f296fa806c90b09beec02
roskzhu <roskenzhu@gmail.com>
2023-12-02 17:53:10 -0500

    landing text and margins

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-white p-4 text-black">
       <div className="container mx-auto">
-        <span className="text-xl font-bold">NavBar</span>
+        <span className="text-xl font-bold">DNA Biotronix</span>
       </div>
     </nav>
   );
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -5,22 +5,20 @@ const Hero = () => {
     <>
       <section
         id="home"
-        className="dark:bg-gray-dark relative z-10 overflow-hidden bg-white pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+        className="dark:bg-gray-dark relative z-10 overflow-hidden bg-white pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px] h-[100vh]"
       >
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div
-                className="wow fadeInUp mx-auto max-w-[700px] text-left"
+                className="wow fadeInUp max-w-[700px] text-left ml-20"
                 data-wow-delay=".2s"
               >
-                <h1 className="mb-5 text-6xl font-bold leading-tight text-black sm:text-4xl sm:leading-tight md:text-6xl md:leading-tight">
-                  Cutting Edge Wearable Nano-Electronic Technology
+                <h1 className="mb-5 text-6xl font-regular leading-tight text-black sm:text-4xl sm:leading-tight md:text-6xl md:leading-tight text-left">
+                Sweat-based Glucose Monitoring with DNA Transistors
                 </h1>
                 <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-gray-500 sm:text-lg md:text-xl">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-                  faucibus nunc lectus, nec mattis enim auctor vel. Sed eget
-                  placerat nulla.
+                Revolutionizing diabetes management
                 </p>
               </div>
             </div>
