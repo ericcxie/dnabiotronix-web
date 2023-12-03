4ec622ee54b17983c7109acdabd4d219b6146ca9
roskzhu <roskenzhu@gmail.com>
2023-12-03 18:09:17 -0500

    landing page

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,6 +1,6 @@
 const About: React.FC = () => {
   return (
-    <div className="bg-white p-8 text-black">
+    <div id="about" className="bg-white p-8 text-black mx-auto">
       <h1 className="text-2xl font-bold">About Section</h1>
     </div>
   );
