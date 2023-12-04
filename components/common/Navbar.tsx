012b77ff1da2be5ccc366a7b08cbee26623c5a21
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-12-03 21:27:45 -0500

    mobile nav

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import React, { useEffect, useState } from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 import FadeUpMotionDiv from "../utils/FadeUpMotion";
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 
 const Navbar: React.FC = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -11,55 +14,93 @@ const Navbar: React.FC = () => {
       setScrolled(isScrolled);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className={`bg-white bg-opacity-30 p-4 text-black pt-5 pl-10 top-0 fixed z-[999] w-full ${scrolled ? 'border-b-[1px] border-gray-200' : ''}`}
-    style={{ backdropFilter: "blur(5px)" }}>
+    <nav
+      className={`bg-white bg-opacity-30 p-4 text-black pt-5 pl-10 top-0 fixed z-[999] w-full ${
+        scrolled ? "border-b-[1px] border-gray-200" : ""
+      }`}
+      style={{ backdropFilter: "blur(5px)" }}
+    >
       <div className="container mx-auto flex justify-between">
         <FadeUpMotionDiv>
-        <div className="justify-start">
-          <span className="text-xl font-bold
-                    bg-gradient-to-r from-blue-500 to-blue-600 text-transparent bg-clip-text">
-            DNA Biotronix
-          </span>
-        </div>
+          <div className="justify-start">
+            <span
+              className="text-xl font-bold
+                    bg-gradient-to-r from-blue-500 to-blue-600 text-transparent bg-clip-text"
+            >
+              DNA Biotronix
+            </span>
+          </div>
         </FadeUpMotionDiv>
-        
-        <div className="justify-end flex pt-0.5 text-gray-600">
-        <FadeUpMotionDiv>
-          <Link to="about" smooth={true} duration={1000} 
-                className="mx-10 text-sm hover:text-black hover:cursor-pointer 
-                 transition duration-300">
-            About
-          </Link>
+
+        <div className="md:hidden">
+          <button onClick={toggleMenu}>
+            {isOpen ? <IoClose size={20} /> : <FaBars size={20} />}
+          </button>
+        </div>
+
+        <div
+          className={`justify-end flex pt-0.5 text-gray-600 ${
+            !isOpen && "hidden"
+          } md:flex`}
+        >
+          <FadeUpMotionDiv>
+            <Link
+              to="about"
+              smooth={true}
+              duration={1000}
+              className="mx-10 text-sm hover:text-black hover:cursor-pointer 
+                 transition duration-300"
+            >
+              About
+            </Link>
           </FadeUpMotionDiv>
           <FadeUpMotionDiv>
-          <Link to="news" smooth={true} duration={1000} 
-            className="mx-10 text-sm hover:text-black hover:cursor-pointer 
-                 transition duration-300">
-            News</Link>
-            </FadeUpMotionDiv>
-            <FadeUpMotionDiv>
-          <Link to="team" smooth={true} duration={1000} 
-           className="mx-10 text-sm hover:text-black hover:cursor-pointer
-           transition duration-300">
-            Team</Link>
-            </FadeUpMotionDiv>
-            <FadeUpMotionDiv>
-          <Link to="contact" smooth={true} duration={1000} 
-            className="mx-10 text-sm hover:text-black hover:cursor-pointer
-            transition duration-300">
-            Contact</Link>
-            </FadeUpMotionDiv>
+            <Link
+              to="news"
+              smooth={true}
+              duration={1000}
+              className="mx-10 text-sm hover:text-black hover:cursor-pointer 
+                 transition duration-300"
+            >
+              News
+            </Link>
+          </FadeUpMotionDiv>
+          <FadeUpMotionDiv>
+            <Link
+              to="team"
+              smooth={true}
+              duration={1000}
+              className="mx-10 text-sm hover:text-black hover:cursor-pointer
+           transition duration-300"
+            >
+              Team
+            </Link>
+          </FadeUpMotionDiv>
+          <FadeUpMotionDiv>
+            <Link
+              to="contact"
+              smooth={true}
+              duration={1000}
+              className="mx-10 text-sm hover:text-black hover:cursor-pointer
+            transition duration-300"
+            >
+              Contact
+            </Link>
+          </FadeUpMotionDiv>
+        </div>
       </div>
-      </div>
-
     </nav>
   );
 };
