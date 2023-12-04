a3d0fbdeb8146f02aa99959974bbf75aff6a51e3
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-12-03 20:10:00 -0500

    Fix landing img load in bug

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,4 +1,4 @@
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link, animateScroll as scroll } from "react-scroll";
 import FadeUpMotionDiv from "../utils/FadeUpMotion";
 
 const Hero = () => {
@@ -18,42 +18,44 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <FadeUpMotionDiv>
-                <h1 className="mt-0 mb-5 text-5xl font-regular leading-tight text-black sm:text-3xl sm:leading-tight md:text-5xl md:leading-tight text-left">
-                Sweat-based Glucose Monitoring with {" "}
-                <span className="font-semibold
-                bg-gradient-to-r from-blue-600 to-blue-400 text-transparent bg-clip-text">
-                  DNA Transistors
-                </span>
-                </h1>
+                  <h1 className="mt-0 mb-5 text-5xl font-regular leading-tight text-black sm:text-3xl sm:leading-tight md:text-5xl md:leading-tight text-left">
+                    Sweat-based Glucose Monitoring with{" "}
+                    <span
+                      className="font-semibold
+                bg-gradient-to-r from-blue-600 to-blue-400 text-transparent bg-clip-text"
+                    >
+                      DNA Transistors
+                    </span>
+                  </h1>
                 </FadeUpMotionDiv>
                 <FadeUpMotionDiv>
-                <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-gray-500 sm:text-lg md:text-xl">
-                Revolutionizing diabetes <br/> management
-                </p>
+                  <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-gray-500 sm:text-lg md:text-xl">
+                    Revolutionizing diabetes <br /> management
+                  </p>
                 </FadeUpMotionDiv>
                 <FadeUpMotionDiv>
-                <Link to="about" smooth={true} duration={1000} 
-                className="text-black border-2 border-black text-sm font-bold
+                  <Link
+                    to="about"
+                    smooth={true}
+                    duration={1000}
+                    className="text-black border-2 border-black text-sm font-bold
                 inline-block px-6 py-3 bg-transparent rounded-full hover:bg-black hover:text-white
-                transition duration-300">
-                  LEARN MORE
-                </Link>
+                transition duration-300"
+                  >
+                    LEARN MORE
+                  </Link>
                 </FadeUpMotionDiv>
               </div>
             </div>
-            <FadeUpMotionDiv>
             <div className="md:w-1/3 lg:w-1/2 px-4 absolute right-0">
               <div className="absolute left-[-70px] top-[-50px] z-[-1] opacity-30 lg:opacity-100 transform translate-x-[0px]">
-                <img
-                  src="/assets/landingImg.svg"
-                  alt="Hero Background"
-                />
+                <FadeUpMotionDiv>
+                  <img src="/assets/landingImg.svg" alt="Hero Background" />
+                </FadeUpMotionDiv>
               </div>
             </div>
-            </FadeUpMotionDiv>
           </div>
         </div>
-        
       </section>
     </>
   );
